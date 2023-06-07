16b4ece4f6b5ba45bccf2b91e230022ae483b9a7
shreya08022004 <135721328+shreya08022004@users.noreply.github.com>
2023-06-07 23:14:19 +0530

    Create 2Sum Problem.java

---
+++ 2Sum Problem.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] res=new int[2];
+        for(int i=0;i<nums.length;i++)
+        {
+            boolean b=false;
+            for(int j=i+1;j<nums.length;j++)
+            {
+                if(nums[i]+nums[j]==target)
+                {
+                    res[0]=i;
+                    res[1]=j;
+                    b=true;
+                    break;
+                }
+            }
+            if(b)
+            break;
+        }
+        return res;
+        
+    }
+}
